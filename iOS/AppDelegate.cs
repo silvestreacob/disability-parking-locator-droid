247fa4ddc6e39dedcb9289f9e0a84d086ed42c3d
lyon1961dev <silvestre.acob@lyon.us.com>
2017-03-15 09:13:10 +0800

    Create new map page

--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -4,15 +4,19 @@ using System.Linq;
 
 using Foundation;
 using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
 
 namespace dpark.iOS
 {
 	[Register("AppDelegate")]
-	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+	public partial class AppDelegate : FormsApplicationDelegate
 	{
 		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
 		{
-			global::Xamarin.Forms.Forms.Init();
+			Forms.Init();
+
+            Xamarin.FormsMaps.Init();
 
 			LoadApplication(new App());
 
